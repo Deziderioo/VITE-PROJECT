2a1d0d18be3b54b4911248ed9835b7046bd47b84
Deziderioo <joaolddeziderio@gmail.com>
2024-05-13 14:44:27 -0300

    header and sidebar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Header } from "../components/Header";
+import "./globalStyle.css";
+import style from "./app.module.css";
+import { Sidebar } from "../components/Sidebar.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={style.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./header.module.css";
+import ignitelogo from "../assets/ignite-logo.svg";
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={ignitelogo} alt="logoIgnite" />
+    </header>
+  );
+}
